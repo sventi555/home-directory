fce6ff565f7fd21824fa7168b87b9ed1d7d461fd
Stephen Edwards <sventi555@gmail.com>
2019-07-11 14:55:49 -0400

    startup script

--- README.md
+++ README.md
@@ -2,14 +2,9 @@
 
 The customized files that mostly live in my home directory.
 
-## "convenience" packages (installed through brew)
-- ctags
-- the_silver_searcher
-- thefuck
-
 ## Setup
-1. Install the _convenience_ packages
-2. Add the font to system fontbook
-3. Set terminal profile as default
-4. Put dot_files in home directory
-5. Install vim plugins by opening vim and running `:PluginInstall`
+1. Run setup.sh
+
+### Mac specific
+1. Add the font to system fontbook
+2. Set terminal profile as default
---
+++ setup.sh
@@ -0,0 +1,39 @@
+#/usr/bin/env sh
+
+SCRIPT_DIR=$(dirname $0)
+
+# setup vim
+
+# copy in the vimrc
+cp $SCRIPT_DIR/dot_files/.vimrc ~/
+
+# create .vim folder if it does not exist
+if [ ! -d ~/.vim ]; then
+    mkdir ~/.vim
+fi
+
+
+# copy over the colors
+cp -r $SCRIPT_DIR/dot_files/.vim/colors ~/.vim
+
+# create bundle folder if it does not exist
+if [ ! -d ~/.vim/bundle ]; then
+    mkdir ~/.vim/bundle
+fi
+
+# clone vundle into bundle folder
+if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
+    git clone git@github.com:VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
+
+# initialize vim plugins
+vim -c PluginInstall -c qa
+
+
+# profile setup
+
+# copy over zshrc
+cp $SCRIPT_DIR/dot_files/.zshrc ~/
+
+# install zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
