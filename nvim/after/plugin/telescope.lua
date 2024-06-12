760bfd24802c5077490e2107117551d85325b3d3
Stephen Edwards <34760298+sventi555@users.noreply.github.com>
2024-06-12 16:30:44 -0400

    telescope layout update

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,5 +1,14 @@
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files)
-vim.keymap.set('n', '<leader>fp', builtin.git_files)
-vim.keymap.set('n', '<leader>fg', builtin.live_grep)
-vim.keymap.set('n', '<leader>fb', builtin.buffers)
+require('telescope').setup({
+  defaults = {
+    sorting_strategy = 'ascending',
+    layout_strategy = 'horizontal',
+    layout_config = {
+      prompt_position = 'top'
+    }
+  }
+})
+vim.keymap.set('n', '<leader>ff', '<Cmd>Telescope find_files<CR>')
+vim.keymap.set('n', '<leader>fp', '<Cmd>Telescope git_files<CR>')
+vim.keymap.set('n', '<leader>fg', '<Cmd>Telescope live_grep<CR>')
+vim.keymap.set('n', '<leader>fb', '<Cmd>Telescope buffers<CR>')
+vim.keymap.set('n', '<leader>fh', '<Cmd>Telescope help_tags<CR>')
