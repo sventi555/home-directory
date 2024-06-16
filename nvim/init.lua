ada72deec978ea6867d767028b81e5052d6727b6
Stephen Edwards <34760298+sventi555@users.noreply.github.com>
2024-06-16 14:48:45 -0400

    improve gitconfig and nvim remaps

--- nvim/init.lua
+++ nvim/init.lua
@@ -29,8 +29,23 @@ vim.opt.shiftwidth = 2
 vim.opt.smartindent = true
 
 -- Remaps
+-- scroll by 3 lines rather than 1
 vim.keymap.set('n', '<C-e>', '3<C-e>')
 vim.keymap.set('n', '<C-y>', '3<C-y>')
+
+-- go back to cursor position after yanking in vmode
+vim.keymap.set('v', 'y', 'ygv<Esc>')
+
+-- center search results
+vim.keymap.set('n', 'n', 'nzz')
+vim.keymap.set('n', 'N', 'Nzz')
+
+-- center after scrolling 
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+
+-- paste without losing results
+vim.keymap.set('x', '<leader>p', '"_dP')
 
 -- Plugins
 vim.call('plug#begin')
