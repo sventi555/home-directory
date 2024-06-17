43e62e8463269195b810807648a2514e027fe17e
Stephen Edwards <34760298+sventi555@users.noreply.github.com>
2024-06-17 17:23:14 -0400

    restore leader window commands by changing gitgutter maps

--- nvim/after/plugin/gitgutter.lua
+++ nvim/after/plugin/gitgutter.lua
@@ -1,1 +1,4 @@
+vim.g.gitgutter_map_keys = 0
 vim.keymap.set('n', '<leader>gs', '<Cmd>GitGutterStageHunk<CR>')
+vim.keymap.set('n', '<leader>gu', '<Cmd>GitGutterUndoHunk<CR>')
+vim.keymap.set('n', '<leader>gp', '<Cmd>GitGutterPreviewHunk<CR>')
