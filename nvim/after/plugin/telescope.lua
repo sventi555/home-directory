02c0c9dd7965d8bec16796eedc90a2afb43f0510
Stephen Edwards <34760298+sventi555@users.noreply.github.com>
2024-06-18 12:40:30 -0400

    improve telescope ui

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,22 +1,29 @@
+local builtin = require('telescope.builtin')
+local actions = require('telescope.actions')
+
 require('telescope').setup({
   defaults = {
     sorting_strategy = 'ascending',
-    layout_strategy = 'horizontal',
+    layout_strategy = 'vertical',
     layout_config = {
+      mirror = true,
       prompt_position = 'top'
     },
     mappings = {
       n = {
-        ['<C-d>'] = require('telescope.actions').delete_buffer
+        ['<C-d>'] = actions.delete_buffer,
+        ['<C-h>'] = actions.which_key
       },
       i = {
-        ['<C-d>'] = require('telescope.actions').delete_buffer
+        ['<C-d>'] = actions.delete_buffer,
+        ['<C-h>'] = actions.which_key
       }
     }
   }
 })
-vim.keymap.set('n', '<leader>ff', '<Cmd>Telescope find_files<CR>')
-vim.keymap.set('n', '<leader>fp', '<Cmd>Telescope git_files<CR>')
-vim.keymap.set('n', '<leader>fg', '<Cmd>Telescope live_grep<CR>')
-vim.keymap.set('n', '<leader>fb', '<Cmd>Telescope buffers<CR>')
-vim.keymap.set('n', '<leader>fh', '<Cmd>Telescope help_tags<CR>')
+
+vim.keymap.set('n', '<leader>ff', builtin.find_files)
+vim.keymap.set('n', '<leader>fp', builtin.git_files)
+vim.keymap.set('n', '<leader>fg', builtin.live_grep)
+vim.keymap.set('n', '<leader>fb', builtin.buffers)
+vim.keymap.set('n', '<leader>fh', builtin.help_tags)
