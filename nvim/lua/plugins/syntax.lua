917936ea70aeaacf73d31e1ddea2d7c915a27727
Stephen Edwards <34760298+sventi555@users.noreply.github.com>
2024-07-17 16:26:24 -0400

    only keep vimdoc syntax parser in treesitter config

--- nvim/lua/plugins/syntax.lua
+++ nvim/lua/plugins/syntax.lua
@@ -4,7 +4,7 @@ return {
     build = ":TSUpdate",
     config = function()
       require('nvim-treesitter.configs').setup({
-        ensure_installed = { "lua", "vimdoc" },
+        ensure_installed = { "vimdoc" },
         auto_install = true,
 
         highlight = {
