249f1fe40248b7d9c97e8e2a691bb8475cebb608
Stephen Edwards <34760298+sventi555@users.noreply.github.com>
2024-07-17 16:20:41 -0400

    change to config initialization for treesitter

--- nvim/lua/plugins/syntax.lua
+++ nvim/lua/plugins/syntax.lua
@@ -2,13 +2,16 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    opts = {
-      auto_install = true,
+    config = function()
+      require('nvim-treesitter.configs').setup({
+        ensure_installed = { "lua", "vimdoc" },
+        auto_install = true,
 
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-      },
-    }
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = false,
+        }
+      })
+    end
   }
 }
