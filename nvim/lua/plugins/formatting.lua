31d9124d6661334c47c0555d1f004487d353f0ad
Stephen Edwards <34760298+sventi555@users.noreply.github.com>
2024-07-19 11:45:11 -0400

    use none-ls instead of conform

--- nvim/lua/plugins/formatting.lua
+++ nvim/lua/plugins/formatting.lua
@@ -7,22 +7,29 @@ return {
     },
   },
   {
-    'stevearc/conform.nvim',
-    opts = {
-      formatters_by_ft = {
-        lua = { 'stylua' },
-        javascript = { 'prettier' },
-        typescript = { 'prettier' },
-        typescriptreact = { 'prettier' },
-        json = { 'prettier' },
-        yaml = { 'prettier' },
-        css = { 'prettier' },
-        scss = { 'prettier' },
-      },
-      format_on_save = {
-        timeout_ms = 500,
-        lsp_format = 'fallback',
-      },
-    },
+    'nvimtools/none-ls.nvim',
+    config = function()
+      local null_ls = require('null-ls')
+
+      local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
+      null_ls.setup({
+        sources = {
+          null_ls.builtins.formatting.stylua,
+          null_ls.builtins.formatting.prettier,
+        },
+        on_attach = function(client, bufnr)
+          if client.supports_method('textDocument/formatting') then
+            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+            vim.api.nvim_create_autocmd('BufWritePre', {
+              group = augroup,
+              buffer = bufnr,
+              callback = function()
+                vim.lsp.buf.format({ async = false })
+              end,
+            })
+          end
+        end,
+      })
+    end,
   },
 }
