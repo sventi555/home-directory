37635e13990013741a15794c90fcc8f038c563e1
Stephen Edwards <34760298+sventi555@users.noreply.github.com>
2025-03-11 01:46:35 -0400

    add oil file browser

--- nvim/lua/custom/plugins/init.lua
+++ nvim/lua/custom/plugins/init.lua
@@ -2,4 +2,24 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  -- file browser
+  {
+    'stevearc/oil.nvim',
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    -- Optional dependencies
+    -- dependencies = { { 'echasnovski/mini.icons', opts = {} } },
+    dependencies = { 'nvim-tree/nvim-web-devicons' }, -- use if you prefer nvim-web-devicons
+    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+    lazy = false,
+    config = function()
+      require('oil').setup({
+        view_options = {
+          show_hidden = true,
+        },
+      })
+      vim.keymap.set('n', '<leader>e', '<CMD>Oil --float<CR>', { desc = '[E]xplore parent directory' })
+    end,
+  },
+}
