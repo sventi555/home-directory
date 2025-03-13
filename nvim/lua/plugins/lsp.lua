a3632b2b97091a7f236c142cbf8090b1b55cf170
Stephen Edwards <34760298+sventi555@users.noreply.github.com>
2025-03-12 20:58:38 -0400

    lspconfig i actually understand

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,34 +1,35 @@
 return {
   {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v3.x',
+    'neovim/nvim-lspconfig',
     dependencies = {
       'williamboman/mason.nvim',
       'williamboman/mason-lspconfig.nvim',
-      'neovim/nvim-lspconfig',
-      'hrsh7th/nvim-cmp',
       'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-nvim-lua',
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
+      'hrsh7th/cmp-cmdline',
+      'hrsh7th/nvim-cmp',
       'L3MON4D3/LuaSnip',
+      'saadparwaiz1/cmp_luasnip',
     },
     config = function()
-      local lsp_zero = require('lsp-zero')
+      local cmp = require('cmp')
+      local cmp_lsp = require('cmp_nvim_lsp')
+      local capabilities =
+        vim.tbl_deep_extend('force', {}, vim.lsp.protocol.make_client_capabilities(), cmp_lsp.default_capabilities())
 
-      lsp_zero.on_attach(function(client, bufnr)
-        -- see :help lsp-zero-keybindings
-        -- to learn the available actions
-        lsp_zero.default_keymaps({ buffer = bufnr })
-      end)
-
-      require('mason').setup({})
+      require('mason').setup()
       require('mason-lspconfig').setup({
         ensure_installed = { 'lua_ls' },
         handlers = {
           function(server_name)
-            require('lspconfig')[server_name].setup({})
+            require('lspconfig')[server_name].setup({ capabilities = capabilities })
           end,
-
-          lua_ls = function()
-            require('lspconfig').lua_ls.setup({
+          ['lua_ls'] = function()
+            local lspconfig = require('lspconfig')
+            lspconfig.lua_ls.setup({
+              capabilities = capabilities,
               settings = {
                 Lua = {
                   diagnostics = {
@@ -39,6 +40,29 @@ return {
             })
           end,
         },
+      })
+
+      local cmp_select = { behavior = cmp.SelectBehavior.Select }
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            require('luasnip').lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+          ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+          ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+          ['<C-l>'] = cmp.mapping.complete(),
+        }),
+        sources = cmp.config.sources({
+          { name = 'nvim_lsp' },
+          { name = 'nvim_lua' },
+          { name = 'luasnip' },
+        }, {
+          { name = 'buffer' },
+        }),
       })
     end,
   },
