0b7deffc8fc5f0a54f5910361503a83ac67935e5
Stephen Edwards <34760298+sventi555@users.noreply.github.com>
2025-03-17 12:14:05 -0400

    add tsserver to lsp setup explicitly

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -21,7 +21,7 @@ return {
 
       require('mason').setup()
       require('mason-lspconfig').setup({
-        ensure_installed = { 'lua_ls' },
+        ensure_installed = { 'lua_ls', 'ts_ls' },
         handlers = {
           function(server_name)
             require('lspconfig')[server_name].setup({ capabilities = capabilities })
