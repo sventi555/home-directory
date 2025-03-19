b259baaf70b9ff4d8b56742d075b5f7f4a7e595d
Stephen Edwards <34760298+sventi555@users.noreply.github.com>
2025-03-19 17:46:33 -0400

    stabilize formatting autocmds

--- nvim/lua/plugins/formatting.lua
+++ nvim/lua/plugins/formatting.lua
@@ -8,24 +8,33 @@ return {
   },
   {
     'stevearc/conform.nvim',
-    opts = {
-      formatters_by_ft = {
-        lua = { 'stylua' },
-        javascript = { 'prettier' },
-        typescript = { 'prettier' },
-        typescriptreact = { 'prettier' },
-        json = { 'prettier' },
-        yaml = { 'prettier' },
-        css = { 'prettier' },
-        scss = { 'prettier' },
-      },
-      format_on_save = {
-        timeout_ms = 500,
-        lsp_format = 'fallback',
-      },
-    },
-    keys = {
-      { '<leader>f', vim.lsp.buf.format, desc = '[F]ormat' },
-    },
+    config = function()
+      require('conform').setup({
+        formatters_by_ft = {
+          lua = { 'stylua' },
+          javascript = { 'prettier' },
+          typescript = { 'prettier' },
+          typescriptreact = { 'prettier' },
+          json = { 'prettier' },
+          yaml = { 'prettier' },
+          css = { 'prettier' },
+          scss = { 'prettier' },
+        },
+      })
+
+      vim.api.nvim_create_autocmd('BufRead', {
+        callback = function(args)
+          vim.keymap.set('n', '<leader>f', function()
+            require('conform').format({ bufnr = args.buf })
+          end, { desc = '[F]ormat' })
+        end,
+      })
+
+      vim.api.nvim_create_autocmd('BufWritePre', {
+        callback = function(args)
+          require('conform').format({ bufnr = args.buf })
+        end,
+      })
+    end,
   },
 }
