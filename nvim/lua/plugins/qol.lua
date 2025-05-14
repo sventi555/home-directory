3bb60c64c5287fd5e25afe66aff263ac484aa02a
Stephen Edwards <34760298+sventi555@users.noreply.github.com>
2025-05-14 17:23:15 -0400

    ts comments

--- nvim/lua/plugins/qol.lua
+++ nvim/lua/plugins/qol.lua
@@ -1,4 +1,9 @@
 return {
   { 'windwp/nvim-autopairs', opts = {} },
   { 'folke/which-key.nvim', event = 'VeryLazy', opts = {} },
+  {
+    'folke/ts-comments.nvim',
+    opts = {},
+    event = 'VeryLazy',
+  },
 }
