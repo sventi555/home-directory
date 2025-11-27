780350eead954e8164e2a4501e9d54167b64d7b2
Stephen Edwards <34760298+sventi555@users.noreply.github.com>
2025-11-26 23:43:51 -0500

    telescope layout improvements

--- nvim/lua/plugins/search.lua
+++ nvim/lua/plugins/search.lua
@@ -8,12 +8,32 @@ return {
     },
     config = function()
       local telescope = require('telescope')
+      local layout = require('telescope.actions.layout')
 
       telescope.setup({
         defaults = {
+          cycle_layout_list = { 'vertical', 'flex' },
           sorting_strategy = 'ascending',
+          layout_strategy = 'flex',
           layout_config = {
+            vertical = {
+              mirror = true,
+            },
+            horizontal = {
+              preview_width = 0.5,
+            },
+            flip_columns = 160,
             prompt_position = 'top',
+            width = function(_, cols, _)
+              return math.min(180, math.floor(cols * 0.95))
+            end,
+            height = 0.95,
+          },
+          mappings = {
+            n = {
+              ['o'] = layout.toggle_preview,
+              ['<C-l>'] = layout.cycle_layout_next,
+            },
           },
         },
       })
@@ -28,7 +48,7 @@ return {
         { '<leader>sf', builtin.find_files, desc = '[S]earch [F]iles' },
         { '<leader>sp', builtin.git_files, desc = '[S]earch [P]roject files' },
         { '<leader>sg', telescope.extensions.live_grep_args.live_grep_args, desc = '[S]earch by [G]rep' },
-        { '<leader>sc', builtin.git_status, desc = '[S]earch git [C]hanges' },
+        { '<leader>sc', builtin.git_status, desc = '[S]earch [C]hanged files' },
         { '<leader>sr', builtin.resume, desc = '[S]earch [R]esume' },
         {
           '<leader>so',
@@ -37,7 +57,9 @@ return {
           end,
           desc = '[S]earch [O]ld files',
         },
-        { '<leader>b', builtin.buffers, desc = '[B]uffers' },
+        { '<leader>sb', builtin.buffers, desc = '[S]earch [B]uffers' },
+        { '<leader>st', builtin.current_buffer_fuzzy_find, desc = '[S]earch [T]his file' },
+        { '<leader>ss', builtin.grep_string, desc = '[S]earch [S]tring' },
       })
     end,
   },
